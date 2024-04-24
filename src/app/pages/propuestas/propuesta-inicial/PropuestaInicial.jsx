428fe38e122c53cc36a6da01a996d76eebe72b0f
Facundodandrea <facundodandrea01@gmail.com>
2024-04-24 18:38:16 -0300

    Modificacion de propuestas educativas

--- src/app/pages/propuestas/propuesta-inicial/PropuestaInicial.jsx
+++ src/app/pages/propuestas/propuesta-inicial/PropuestaInicial.jsx
@@ -1,6 +1,10 @@
 import Layout from '../../../layout/common/Layout'
 import '../../propuestas/propuesta.css'
 
+import inicial1 from '../../../../assets/imgs/inicial1.jpeg'
+import inicial2 from '../../../../assets/imgs/inicial2.jpeg'
+import inicial3 from '../../../../assets/imgs/Inicial3.jpeg'
+
 const PropuestaInicial = () => {
   return (
     <Layout>
@@ -11,13 +15,15 @@ const PropuestaInicial = () => {
             </h2>
             <h3 className="proposedSubTitle">OBJETIVOS</h3>
             <p>La educación en el Nivel inicial es obligatoria, integral e inclusiva. Buscamos promover en los alumnos la solidaridad, confianza y amistad. El juego, como recurso pedagógico favorece el desarrollo de sus capacidades expresivas, creativas, expresión corporal. Dentro de las salas se promueve el diálogo y el trabajo colaborativo. </p>
-            <img src="" alt="" />
+            <img src={inicial1} alt="" />
             <h3 className="proposedSubTitle">ORGANIZACIÓN ESCOLAR</h3>
             <p>El Nivel Inicial se organiza:</p>
             <p>TURNO MAÑANA 2 divisiones de 5 años </p>
             <p>TURNO TARDE 2 divisiones de 5 años</p>
             <p>Se desarrolla la enseñanza a partir de los diseños jurisdiccionales y de los Núcleos de Aprendizajes Prioritarios.</p>
             <p>Promueve el juego como contenido transversal, con propuestas pedagógicas que favorezcan el crecimiento de nuestros alumnos a lo largo de su trayectoria escolar. La propuesta esta articulada con Educación Musical y Educación Física y el Nivel Primario.</p>
+            <img src={inicial2} alt="" />
+            <img src={inicial3} alt="" />
             </div>
         </div>
     </Layout>
--- src/app/pages/propuestas/propuesta-primaria/PropuestaPrimaria.jsx
+++ src/app/pages/propuestas/propuesta-primaria/PropuestaPrimaria.jsx
@@ -1,6 +1,9 @@
 import Layout from '../../../layout/common/Layout'
 import '../../propuestas/propuesta.css'
 
+import primario1 from '../../../../assets/imgs/Primario1.jpeg'
+import primario2 from '../../../../assets/imgs/Primario2.jpeg'
+
 const PropuestaPrimaria = () => {
   return (
     <Layout>
@@ -11,12 +14,13 @@ const PropuestaPrimaria = () => {
             </h2>
             <h3 className="proposedSubTitle">OBJETIVOS</h3>
             <p>La educación en el nivel primario es obligatoria y constituye una unidad pedagógica y organizativa, destinada a la formación integral de niñas y niños a partir de los 6 hasta los 11 años de edad. La misma garantiza el acceso a saberes , prácticas y experiencias culturales relevantes para que puedan intervenir progresivamente de diferentes maneras, participar en una sociedad democrática, promover el desarrollo de la personalidad, el pensamiento crítico, la solidaridad social y  el juicio moral autónomo de los alumnos incrementando su capacidad de conocerse y cambiar, de conocer el mundo e influir en él, garantizar el dominio por parte de todos los alumnos de las herramientas necesarias para continuar su aprendizaje más allá de la educación básica.  Además, contamos con espacios de participación como “la radio escolar”, “mediación escolar”, “patrulla ambiental”.</p>
-            <img src="" alt="" />
+            <img src={primario1} alt="" />
             <h3 className="proposedSubTitle">ORGANIZACIÓN ESCOLAR</h3>
             <p>El Nivel Primario se organiza por ciclos:</p>
             <p>1er ciclo: 1°,2° y 3° grado con tres divisiones de 1° grado en el turno mañana, las demás con dos divisiones</p>
             <p>2do ciclo: 4°, 5° y 6° grado con tres divisiones en 6° grado en el turno tarde, las demás con dos divisiones.</p>
             <p>Desarrollando una propuesta educativa según los siguientes espacios curriculares: Matemática, Lengua, Ciencias Naturales, Ciencias Sociales, Tecnología, Educación Plástica, Educación Musical, Educación Física, Lengua Extranjera (inglés) y Religión, respondiendo a los diseños jurisdiccionales y a los Núcleos de Aprendizajes Prioritarios (NAP), definidos por el Ministerio de Educación de la Nación y de la provincia.</p>
+            <img src={primario2} alt="" />
             </div>
         </div>
     </Layout>
